a8cd071ec27fa740c97d8a3fb52978f58f3eb12c
gabrielly-soares-marinho <gabrielly.soares05marinho@gmail.com>
2025-04-03 16:46:53 -0300

    Update index.js
    alterações

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-async function save(){
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let usertype = 1;
-    let password = document.getElementById("password").value;
-    let termos = 1;
-    let birthday = document.getElementById("data").value;
-    let cpfcnpj = document.getElementById("cpf").value;
+document.getElementById("cadastro").addEventListener("submit", async function(event) {
+    event.preventDefault(); 
+    await save();
+});
 
-dados = {       
-    "name": name,
-    "email": email,
-    "user_type_id": usertype,
-    "password": password,
-    "cpf_cnpj": cpfcnpj,
-    "terms": termos,
-    "birthday": birthday
-}
-    let api = await fetch(
-        "https://go-wash-api.onrender.com/api/user",{
-            method:"POST",
-            body:JSON.stringify(dados),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+async function save() {
+    try {
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let usertype = 1;
+        let password = document.getElementById("password").value;
+        let termos = 1;
+        let birthday = document.getElementById("data").value;
+        let cpfcnpj = document.getElementById("cpf").value;
+
+        let dados = {       
+            "name": name,
+            "email": email,
+            "user_type_id": usertype,
+            "password": password,
+            "cpf_cnpj": cpfcnpj,
+            "terms": termos,
+            "birthday": birthday
+        };
+
+        let api = await fetch("https://go-wash-api.onrender.com/api/user", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(dados)
+        });
+
+        let result = await api.json();
+
+        if (api.ok) {
+            console.log("Cadastro realizado com sucesso!", result);
+        } else {
+            console.error("Erro ao cadastrar:", result);
         }
-    );
 
-    if(api.ok){
-        let response = await api.json()
-        console.log(response);
-        return
+    } catch (error) {
+        console.error("Erro inesperado:", error);
     }
-    let responseErro = await api.json()
-    console.log(responseErro);
 }
-
