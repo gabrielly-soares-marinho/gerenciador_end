825c578b900efdd63c7df962c05bb7e98959ae1c
emi-Dev <emillysilvaduardo@gmail.com>
2025-05-13 22:02:59 -0300

    login

--- login.js
+++ login.js
@@ -1,21 +1,28 @@
-async function login (){
-    let url = ""
-    let api = await fetch(url,{
+document.getElementById("login").addEventListener("submit", async function (event) {
+    event.preventDefault();
+    await save();
+});
 
+
+async function login() {
+    let url = "https://go-wash-api.onrender.com/api/login"
+    let api = await fetch(url, {
+        method: "POST",
+        body: {
+            "email": "",
+            "password": "",
+            "user_type_id": 1
+        },
+        headers: { "Content-Type": "application/json" },
     })
 
-    if(api.ok)(
+    if (api.ok) {
         let response = await api.json()
-        console.log(response)
-        localStorage.setItem("user", JSON.stringify(response))
-        alert ("login com sucesso")
-    }else{
+    console.log(response)
+    localStorage.setItem("user", JSON.stringify(response))
+    alert("login com sucesso")
+}else {
     let responseError = await api.json()
     console.log(responseError)
 }
-
-//mandar informação
-function getUserData(){
-    let user = localStorage.getIt("nome")
-    console.log(user)
 }
